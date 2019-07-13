1ee3ba47f10a22db16dd1073b02ffbf6a5cd6051
Nitin Ashok Pawar <npawar@altimetrik.com>
2019-07-13 13:36:26 +0530

    adds eureka and commented hard coded list of servers

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.example")
 @RibbonClient(name="currency-exchange-service")
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
